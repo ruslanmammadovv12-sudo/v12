c6e7249f02960ae954398d0e1c20ecc9541a554c
[dyad] <ruslan.mammadov.v12@gmail.com>
2025-11-07 11:53:26 +0400

    [dyad] wrote 1 file(s)

--- src/pages/OutgoingPayments.tsx
+++ src/pages/OutgoingPayments.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import { useData, Payment, PurchaseOrder } from '@/context/DataContext';
 import { t } from '@/utils/i18n';
 import { Button } from '@/components/ui/button';
